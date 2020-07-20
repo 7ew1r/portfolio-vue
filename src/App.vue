260cdf9ff2fde4c264b4774176eb2ad65e9f2665
7ew1r <art.sht.0317@gmail.com>
2020-07-20 16:45:56 +0900

    :sparkles: feat: update iroiro

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
   <!-- NavBar -->
-    <b-navbar toggleable="lg" type="light" variant="light" sticky>
-      <b-navbar-brand :href="url+'#'">7ew1r.github.io</b-navbar-brand>
+
+    <b-navbar class="ml-auto" toggleable="lg" type="light" variant="light" sticky>
+      <b-container>
+      <b-navbar-brand class="navbar-title" :href="url+'#'">7ew1r.github.io</b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :href="url+'#biography'">Biography</b-nav-item>
-          <b-nav-item :href="url+'#works'">Works</b-nav-item>
-          <b-nav-item :href="url+'#contact'">Contact</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item :href="url+'#biography'">Biography</b-nav-item>
+            <b-nav-item :href="url+'#works'">Works</b-nav-item>
+            <b-nav-item :href="url+'#contact'">Contact</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar><!-- NavBar -->
+
     <router-view/>
     <footer><small>&copy; 2020 tewi_r</small></footer>
   </div>
@@ -19,12 +23,18 @@
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Raleway');
+@import url('https://fonts.googleapis.com/css?family=Poppins');
+body {
+  background-color: #161E27;
+}
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif, 'Raleway';
+  font-family: 'Avenir', Helvetica, Arial, sans-serif, 'Raleway', 'Poppins';
+  font-size: 1.2rem;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+
+  color: #f8f9fa;
 }
 nav {
   filter: drop-shadow(0 5px 10px rgba(0,0,0,0.4));
@@ -36,11 +46,17 @@ nav {
   margin-bottom: 5em;
 }
 .section-title {
-  margin: 0.5em 0;
-  font-family: 'Raleway', sans-serif;
+  font-size: 3.0rem;
+  color: #BA1234;
+  margin: 1.5rem 0;
+  font-family: 'Poppins', 'Raleway', sans-serif;
 }
 footer {
   text-align: center;
+}
+
+a {
+  color: #BA1234;
 }
 </style>
 
